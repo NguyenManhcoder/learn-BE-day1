ed7d2ce36833f727b25b9cf585f786e44442a69e
Mạnh đêy <nguyenmanhbdh2004@gmail.com>
2024-12-11 06:18:37 +0700

    Update bai8.js

--- learn-git-01/bai8.js
+++ learn-git-01/bai8.js
@@ -3,12 +3,10 @@ const arr = [1, 2, 3, 2, 1, 2]
 function findMostFrequent(arr){
     let frequency = {};
 
-  // Duyệt qua mảng và cập nhật tần suất vào đối tượng
   for (let num of arr) {
     frequency[num] = (frequency[num] || 0) + 1;
   }
 
-  // Tìm phần tử có tần suất cao nhất
   let mostFrequentElement = null;
   let maxFrequency = 0;
 
